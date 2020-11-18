2308da46b48d2e84671dafa4f5eabfb846171593
martin <martin.kubo8@gmail.com>
2020-11-18 19:51:02 +0100

    php engine setup + pages needed

--- index.php
+++ index.php
@@ -1,7 +1,24 @@
 <?php 
+  $page = isset($_GET['page']) ? $_GET['page'] : 'index';
+  
+  
   include 'header.php';
 
+  echo '</br>';
 
+
+  switch($page) {
+    case 'cuisine': 
+      include 'cuisine.php';
+      break;
+    case 'index':
+      include 'frontpage.php';
+      break;
+    default: 
+      include '404.php';
+      break;
+  }
+  echo '</br>';
 
   include 'footer.php';
 ?>
