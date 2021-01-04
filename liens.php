b3942e8a1c2723d5435d2074473352bd6d085c26
martinjakubec <martin.kubo8@gmail.com>
2021-01-04 13:53:59 +0100

    clearup

--- liens.php
+++ liens.php
@@ -4,7 +4,7 @@ require_once 'link_form.php';
 
 
 <?php
-if(file_exists('links.txt')) {
+if (file_exists('links.txt')) {
   $readingLinkFile = fopen('links.txt', 'r');
   $links = fread($readingLinkFile, filesize('links.txt') === 0 ? 1 : filesize('links.txt'));
   $links = explode('ENDOFENTRY', $links);
@@ -14,14 +14,14 @@ if(file_exists('links.txt')) {
     }
     $linkParts = explode(' --- ', $singleLink);
     list($name, $link, $comment, $time) = $linkParts;
-  ?>
+?>
     <div style="border: 1px solid black;">
       <p>Nom: <span><?php echo $name ?></span></p>
-      <p>Lien: <span><a href="<?php 
-        if (substr($link, 0, 7) != 'http://' || substr($link, 0, 8) != 'https://') {
-          echo 'http://' . $link;
-        }
-      ?>"><?php echo $link ?></a></span></p>
+      <p>Lien: <span><a href="<?php
+                              if (substr($link, 0, 7) != 'http://' || substr($link, 0, 8) != 'https://') {
+                                echo 'http://' . $link;
+                              }
+                              ?>"><?php echo $link ?></a></span></p>
       <p>Commentaire: <span><?php echo $comment ?></span></p>
       <p>Date: <span><?php echo date('d/m/Y', $time) ?></span></p>
     </div>
@@ -31,12 +31,8 @@ if(file_exists('links.txt')) {
 } else {
   ?>
   <div style="border: 1px solid black;">
-      <p>Pas de lien pour l'instant</p>
-    </div>
-    <?php
+    <p>Pas de lien pour l'instant</p>
+  </div>
+<?php
 }
-
 ?>
-
-
-
