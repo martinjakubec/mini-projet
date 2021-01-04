c8b55dca38aad391373c8ed4934cf8147fe884dc
martinjakubec <martin.kubo8@gmail.com>
2021-01-04 13:25:27 +0100

    customize link form

--- link_form.php
+++ link_form.php
@@ -1,8 +1,8 @@
 <form action="" method="POST">
   <label for="name">Votre nom:</label>
-  <input type="text" name="name">
+  <input type="text" name="name" required>
   <label for="link">Votre lien:</label>
-  <input type="text" name="link">
+  <input type="text" name="link" required>
   <label for="commment">Commentaire:</label>
   <textarea name="comment" cols="30" rows="10" placeholder="Pourquoi trouvez-vous que votre lien est util ? (max. 200 caractères)" maxlength="200"></textarea>
   <input type="submit" value="Envoyer">
@@ -20,9 +20,9 @@ if (isset($_POST['name']) && isset($_POST['link'])) {
   $comment = htmlentities($comment);
   $newTime = time();
 
-  
-    $linkfile = fopen('links.txt', 'a+');
-    fwrite($linkfile, "$name --- $link --- $comment --- $newTime" . 'ENDOFENTRY'. PHP_EOL);
-    fclose($linkfile);
+  $linkfile = fopen('links.txt', 'a+');
+  fwrite($linkfile, "$name --- $link --- $comment --- $newTime" . 'ENDOFENTRY' . PHP_EOL);
+  fclose($linkfile);
+  header("Location: /mini-projet/?page=liens");
 }
 ?>
