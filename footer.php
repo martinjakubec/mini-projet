6656f42275d83cd540dce7a7b826aeed94af3a96
martinjakubec <martin.kubo8@gmail.com>
2021-01-11 18:06:15 +0100

    css page liens + global

--- footer.php
+++ footer.php
@@ -1,5 +1,7 @@
 <footer id="footer">
-  <a href="#" class="underline-link" onclick="changeCss('style1')">CSS1</a>
-  <a href="#" class="underline-link" onclick="changeCss('style2')">CSS2</a>
-  <a href="#" class="underline-link" onclick="changeCss('style3')">Pas de style</a>
+  <div class="container">
+    <a href="#" class="underline-link" onclick="changeCss('style1')">CSS1</a>
+    <a href="#" class="underline-link" onclick="changeCss('style2')">CSS2</a>
+    <a href="#" class="underline-link" onclick="changeCss('style3')">Pas de style</a>
+  </div>
 </footer>
