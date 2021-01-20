bf195faae7d0d4aa365fd48e05ba13127c6510ad
martinjakubec <martin.kubo8@gmail.com>
2021-01-20 18:51:14 +0100

    ajout de fonctionnalités css

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 <footer id="footer">
   <div class="container">
-    <a href="#" class="underline-link" onclick="changeCss('style1')">CSS1</a>
-    <a href="#" class="underline-link" onclick="changeCss('style2')">CSS2</a>
-    <a href="#" class="underline-link" onclick="changeCss('style3')">Pas de style</a>
+    <a href="#" class="menu-button" onclick="changeCss('style1')"><span class="menu-button__text">CSS1</span></a>
+    <a href="#" class="menu-button" onclick="changeCss('style2')"><span class="menu-button__text">CSS2</span></a>
+    <a href="#" class="menu-button" onclick="changeCss('style3')"><span class="menu-button__text">Pas de style</span></a>
   </div>
 </footer>
